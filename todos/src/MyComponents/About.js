59e5d6b4c5166330af937e1d9130a80a25846fd9
N Sonam <sonamneelam037@gmail.com>
2021-09-05 14:03:23 +0530

    Added About page to todos

---
+++ todos/src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is About Component'
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+        perferendis laudantium? Culpa, eum. Recusandae excepturi tempore facilis
+        impedit non commodi velit, expedita doloremque sapiente odio eius
+        repellendus dolorem blanditiis tempora omnis officiis nulla vero.
+      </p>
+    </div>
+  );
+};
