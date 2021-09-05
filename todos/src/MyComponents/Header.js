b9c3a56918050d51220721ec444ea9092ee6af3a
N Sonam <sonamneelam037@gmail.com>
2021-09-05 14:56:13 +0530

    Optimized css

--- todos/src/MyComponents/Header.js
+++ todos/src/MyComponents/Header.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function Header(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+      <div className="container-fluid mx-5 p-3 px-5">
         <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
