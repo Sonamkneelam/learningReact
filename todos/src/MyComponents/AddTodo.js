3b02d32e53ab37af13c0104aa91065af1372af10
N Sonam <sonamneelam037@gmail.com>
2021-09-05 14:46:46 +0530

    Added component

---
+++ todos/src/MyComponents/AddTodo.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export const AddTodo = ({ addTodo }) => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!title || !desc) {
+      console.log(title);
+      alert("Title or desc not defined");
+    } else {
+      addTodo(title, desc);
+      setTitle("");
+      setDesc("");
+    }
+  };
+  return (
+    <div className="container my-3">
+      <h3>Add a Todo</h3>
+      <form onSubmit={submit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Todo Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            aria-describedby="emailHelp"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            ToDo Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="desc"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-sm btn-success">
+          Add ToDo
+        </button>
+      </form>
+    </div>
+  );
+};
