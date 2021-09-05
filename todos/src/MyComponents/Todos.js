0b4a8742dfec52bedfa8d13ffb0df4c1ad1c544d
N Sonam <sonamneelam037@gmail.com>
2021-09-05 14:47:13 +0530

    Added component Todos

---
+++ todos/src/MyComponents/Todos.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { TodoItem } from "./TodoItem";
+
+export const Todos = (props) => {
+  let myStyle = {
+    minHeight: "70vh",
+    margin: "40px auto",
+  };
+  return (
+    <div className="container my-3" style={myStyle}>
+      <h3 className="my-3">Todos List</h3>
+      {props.todos.length === 0
+        ? "No Todos to dosplay"
+        : props.todos.map((todo) => {
+            return (
+              <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+            );
+            // return (
+            // <>
+            // <h3>this</h3>
+            //  <TodoItem todo={todo}/>
+            //  </?>
+            //       )
+          })}
+    </div>
+  );
+};
